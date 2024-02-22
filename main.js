277a115e365e4d8428dddf0ed04f3688b46e9b95
MaksymKurbesov <bonyklade@gmail.com>
2024-02-22 11:59:41 +0200

    updated backend

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ import path from "path";
 import cors from "cors";
 import fs from "fs";
 import { dirname } from "path";
+import https from "https";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+const httpsOptions = {
+  cert: fs.readFileSync("ssl/apate.crt"),
+  key: fs.readFileSync("ssl/apate.key"),
+  ca: fs.readFileSync("ssl/apate.ca-bundle"),
+};
 
 app.get("/add-users", async (req, res) => {
   const workbook = new Excel.Workbook();
@@ -21,6 +29,8 @@ app.get("/add-users", async (req, res) => {
   // res.send("Hello World123");
 });
 
-app.listen(3000);
+https.createServer(httpsOptions, app).listen(3000, () => {
+  console.log("listen2");
+});
 
 export const viteNodeApp = app;
