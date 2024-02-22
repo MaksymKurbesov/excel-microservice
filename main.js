e5398b1bf2f777ad7d28f0d547d5d65ae73118e1
MaksymKurbesov <bonyklade@gmail.com>
2024-02-22 11:43:38 +0200

    init

---
+++ main.js
@@ -0,0 +1,26 @@
+import express from "express";
+import Excel from "exceljs";
+import path from "path";
+import cors from "cors";
+import fs from "fs";
+import { dirname } from "path";
+
+const app = express();
+app.use(cors());
+
+app.get("/add-users", async (req, res) => {
+  const workbook = new Excel.Workbook();
+  const sheet = workbook.addWorksheet("Participants");
+
+  sheet.getCell(`A1`).value = `test`;
+
+  const fileName = "Participants.xlsx";
+  await workbook.xlsx.writeFile(fileName);
+
+  res.sendFile(fileName, { root: path.resolve() });
+  // res.send("Hello World123");
+});
+
+app.listen(3000);
+
+export const viteNodeApp = app;
